d40c27a5c726afed5d4a1f58b9c6be06d1178e3f
Akshay Khot <akshay.7277@gmail.com>
2021-04-13 19:06:16 +0530

    cleanup

--- src/ninja.js
+++ src/ninja.js
@@ -1,3 +1,15 @@
 export class Ninja {
-  message = 'Welcome to JavaScript Ninja...';
+  message = 'Secrets of the JavaScript Ninja';
+  a = 0;
+  b = 0;
+  sum = 0;
+
+  constructor() {
+	  
+  }
+
+  add() {
+	this.sum = Number(this.a) + Number(this.b);
+  }
+
 }
